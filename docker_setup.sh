ec5067032eeb7e87d0930d9489c21064608b83ca
Roniel Valdez <roniel_valdez@outlook.com>
2023-06-17 16:08:27 -0400

    chore: rename files

--- docker_setup.sh
+++ docker_setup.sh
@@ -3,7 +3,7 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker $USER
-sudo reboot
-
 # Create pi-network
 docker network create --driver bridge pi-network
+sudo reboot
+

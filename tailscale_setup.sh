cd4cd94727bba32263803d94a91c1e9f9976510b
Roniel Valdez <roniel_valdez@outlook.com>
2023-06-21 00:08:14 -0400

    fix: typo

--- tailscale_setup.sh
+++ tailscale_setup.sh
@@ -3,5 +3,5 @@
 curl -fsSL https://tailscale.com/install.sh | sh
 
 # start tailscale using the configured DNS on the admin panel
-sudo tailscale up --acept-dns=false
+sudo tailscale up --accept-dns=false
 

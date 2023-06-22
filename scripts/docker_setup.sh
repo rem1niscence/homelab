ed16dfe76bc6f9fbf4ba193b14cdd01510a5b281
Roniel Valdez <roniel_valdez@outlook.com>
2023-06-22 00:07:34 -0400

    revert: remove fm path

--- scripts/docker_setup.sh
+++ scripts/docker_setup.sh
@@ -5,3 +5,6 @@ sudo sh get-docker.sh
 sudo docker network create --driver bridge pi-network
 sudo usermod -aG docker $USER
 sudo reboot
+
+# If you want a quick ephemeral image:
+# docker run --rm -it --entrypoint bash ubuntu

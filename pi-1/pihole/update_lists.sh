a996f75d382c2d31b340aa57afd0fb058c9cb993
Roniel Valdez <roniel_valdez@outlook.com>
2023-06-23 20:14:43 -0400

    docs: add block list to update script

--- pi-1/pihole/update_lists.sh
+++ pi-1/pihole/update_lists.sh
@@ -3,4 +3,7 @@
 docker container exec -t pihole pihole -g
 
 # cron to run it once a day
-# 15 3 * * * /home/pi-1/pihole/update_lists.sh
+# 15 3 * * * ~/pihole/update_lists.sh
+
+# Blocklists
+# https://github.com/blocklistproject/Lists

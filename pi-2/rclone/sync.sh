5b11f8995a1408e5a74cd7fb9288102282b5b12a
Roniel Valdez <roniel_valdez@outlook.com>
2023-07-05 20:39:41 -0400

    feat: add ttl to rclone job

--- pi-2/rclone/sync.sh
+++ pi-2/rclone/sync.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-docker container exec rclone rclone sync onedrive: /data/onedrive
-docker container exec rclone rclone sync gphotos: /data/gphotos
-docker container exec rclone rclone sync gdrive: /data/gdrive
+# 15 min timeout for each sync
+timeout 900 docker container exec rclone rclone sync onedrive: /data/onedrive
+docker 900 container exec rclone rclone sync gphotos: /data/gphotos
+docker 900 container exec rclone rclone sync gdrive: /data/gdrive
 
 # cron to run it once a day
 # 30 4 * * * ~/rclone/sync.sh

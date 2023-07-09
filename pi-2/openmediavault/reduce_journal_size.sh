30c1e030d13e81f946fa9aa3d8eb7ed47d5550e6
Roniel Valdez <roniel_valdez@outlook.com>
2023-07-09 00:59:45 -0400

    feat: improve flush ram script

--- pi-2/openmediavault/reduce_journal_size.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-journalctl --vacuum-size=10M
-
-# 0 5 * * * ~/reduce_journal_size.sh
-
-# Remember to reinstall folder2ram every once in a while to prevent the plugin
-# of eating all your RAM. 

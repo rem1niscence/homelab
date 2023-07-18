4cfcaa494d8ae62cca6828cad34e0fcb7b6bc60b
Roniel Valdez <roniel_valdez@outlook.com>
2023-07-18 02:31:12 -0400

    feat: makefile setup for services/addons

---
+++ kubernetes/setup_application.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+path=$1
+domain=$2
+volume=$3
+
+echo "setting up $path"
+
+set -o allexport
+source "./$path/.env"
+set +o allexport
+
+envsubst < "./$path/secrets.yml" > "./$path/secrets-processed.yml"
+
+kubectl apply -f "./$path/secrets-processed.yml"
+
+if [ "$volume" = "true" ]; then
+    kubectl apply -f "./$path/pv.yml"
+    kubectl apply -f "./$path/pvc.yml"
+fi
+
+kubectl apply -f "./$path/deployment.yml"
+kubectl apply -f "./$path/service.yml"
+sed -e "s;{{DOMAIN}};$domain;g" "./$path/ingress-route.yml" | kubectl apply -f -
+
+echo "done ✅"

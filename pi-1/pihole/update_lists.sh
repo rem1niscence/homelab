65a9476f25749d49e0333fea0644c5d4164193ec
Roniel Valdez <roniel_valdez@outlook.com>
2023-07-29 21:24:56 -0400

    chore: delete unused/imported apps

--- pi-1/pihole/update_lists.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker container exec -t pihole pihole -g
-
-# cron to run it once a day
-# 15 3 * * * ~/pihole/update_lists.sh
-
-# Blocklists
-# https://github.com/blocklistproject/Lists
--- pi-2/openmediavault/openmediavault_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash
-
-# Note: after this is done it will break all the network connections and change
-# the hostname to the user name, you may have to perform manual network changes
-# TODO: Explain how to fix such changes

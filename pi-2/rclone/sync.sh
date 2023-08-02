779747d31ec9364480c02daa41627806da35d7b6
Roniel Valdez <roniel_valdez@outlook.com>
2023-08-01 23:19:21 -0400

    feat: rclone

--- pi-2/rclone/sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 15 min timeout for each sync
-timeout 900 docker container exec rclone rclone sync onedrive: /data/onedrive
-docker 900 container exec rclone rclone sync gphotos: /data/gphotos
-docker 900 container exec rclone rclone sync gdrive: /data/gdrive
-
-# cron to run it once a day
-# 30 4 * * * ~/rclone/sync.sh

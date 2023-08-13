0c038e4909f6e929925478724f0b6c972feb9269
Roniel Valdez <roniel_valdez@outlook.com>
2023-08-12 22:23:33 -0400

    feat: add encyption setup to k3s

--- scripts/add_k3s_node.sh
+++ scripts/add_k3s_node.sh
@@ -4,5 +4,5 @@ if [[ -z "${IP}" ]] || [[ -z "${TOKEN}" ]]; then
     echo "Environment variables IP and TOKEN must be set"
     exit 1
 else
-    curl -sfL https://get.k3s.io | K3S_URL=https://$(IP):6443 K3S_TOKEN=$(TOKEN) sh -s -
+    curl -sfL https://get.k3s.io | K3S_URL=https://$(IP):6443 K3S_TOKEN=$(TOKEN) sh -s - server --secrets-encryption
 fi

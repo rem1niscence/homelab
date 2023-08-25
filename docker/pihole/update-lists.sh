608355bf5b6e1987d8922b2f505e835ea3eaa027
Roniel Valdez <roniel_valdez@outlook.com>
2023-08-24 23:20:59 -0400

    Even more services because I need help (#25)
    * service: whoogle
    
    * service: tandoor
    
    * feat: restore pihole to k3s
    
    * feat: linkding
    
    * feat: replace tandoor for kitchen owl
    
    * chore: yamlfmt
    
    * feat: excalidraw
    
    * feat: cloudbeaver

--- docker/pihole/update-lists.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker container exec -t pihole pihole -g
-
-# cron to run it once a day
-# 15 3 * * * ~/docker/pihole/update_lists.sh
-
-# Blocklists
-# https://blocklistproject.github.io/Lists/tracking.txt

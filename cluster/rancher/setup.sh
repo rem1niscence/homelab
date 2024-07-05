1f296966dc8f5f2d2a12c13fc363d35d88b13a42
Roniel Valdez <roniel_valdez@outlook.com>
2024-07-05 18:42:50 +0200

    chore: change script

--- cluster/rancher/setup.sh
+++ cluster/rancher/setup.sh
@@ -4,7 +4,7 @@ hostname=$1
 
 helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 kubectl create namespace cattle-system
-helm install rancher rancher-latest/rancher \
+helm upgrade rancher rancher-latest/rancher \
   --namespace cattle-system \
   --set hostname=${hostname} \
   --set replicas=3

36673c6bebf0f0bc09fac6d501a6ba7ac2c9c390
Roniel Valdez <roniel_valdez@outlook.com>
2025-01-21 16:28:23 +0100

    feat: set up kubevirt and a vm

---
+++ cluster/kubevirt/setup-kubevirt-manager.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+kubectl apply -f https://raw.githubusercontent.com/kubevirt-manager/kubevirt-manager/main/kubernetes/bundled.yaml
+    
---
+++ cluster/kubevirt/setup-kubevirt.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Point at latest release
+export RELEASE=$(curl https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirt/stable.txt)
+# Deploy the KubeVirt operator
+kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/kubevirt-operator.yaml
+# Create the KubeVirt CR (instance deployment request) which triggers the actual installation
+kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/kubevirt-cr.yaml
+# wait until all KubeVirt components are up
+kubectl -n kubevirt wait kv kubevirt --for condition=Available
+
+# Install virtctl
+export VERSION=$(curl https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirt/stable.txt)
+wget https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/virtctl-${VERSION}-linux-amd64
+mv virtctl-${VERSION}-linux-amd64 virtctl
+mv virtcl /usr/bin
+
+# Install CDI
+export VERSION=$(curl -s https://api.github.com/repos/kubevirt/containerized-data-importer/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-operator.yaml
+kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-cr.yaml

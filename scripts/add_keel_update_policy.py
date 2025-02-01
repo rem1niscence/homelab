23078c5270f18e1d493da6b5dd854355ce01501b
Roniel Valdez <roniel_valdez@outlook.com>
2025-02-01 02:07:00 +0100

    fix: check for apiexception

--- scripts/add_keel_update_policy.py
+++ scripts/add_keel_update_policy.py
@@ -1,25 +1,34 @@
 from kubernetes import client, config
+from kubernetes.client.exceptions import ApiException
 
 config.load_kube_config()
 v1 = client.AppsV1Api()
 
-skipped_namespaces = ["kube-system",
-                      "cert-manager",
-                      "default",
-                      "monitoring",
-                      "tailscale",
-                      "longhorn-system",
-                      "cattle-system",
-                      "cattle-fleet-system",
-                      "cattle-fleet-local-system"]
-namespaces = [ns.metadata.name for ns in client.CoreV1Api().list_namespace().items
-              if ns.metadata.name not in skipped_namespaces]
+skipped_namespaces = [
+    "kube-system",
+    "cert-manager",
+    "default",
+    "monitoring",
+    "tailscale",
+    "cdi",
+    "longhorn-system",
+    "cattle-system",
+    "cattle-fleet-system",
+    "kubevirt",
+    "cattle-fleet-local-system",
+]
+namespaces = [
+    ns.metadata.name
+    for ns in client.CoreV1Api().list_namespace().items
+    if ns.metadata.name not in skipped_namespaces
+]
+
 
 # Define the annotations
 annotations_to_add = {
     "keel.sh/policy": "all",
     "keel.sh/pollSchedule": "@every 1h",
-    "keel.sh/trigger": "poll"
+    "keel.sh/trigger": "poll",
 }
 
 # Iterate over namespaces
@@ -28,7 +37,8 @@ for namespace_name in namespaces:
 
     # Iterate over deployments in the current namespace
     for dep in deployments.items:
-        # Ensure the deployment has an annotations attribute, or create an empty one
+        # Ensure the deployment has an annotations attribute, or create an
+        # empty one
         if not dep.metadata.annotations:
             dep.metadata.annotations = {}
 
@@ -36,14 +46,29 @@ for namespace_name in namespaces:
         modified = False
         for key, value in annotations_to_add.items():
             if key in dep.metadata.annotations:
-              continue
+                continue
             dep.metadata.annotations[key] = value
             modified = True
 
         if not modified:
+            print(
+                f"Skipping deployment '{dep.metadata.name}' in the"
+                + f"'{namespace_name}' namespace."
+            )
             continue
 
-        # Update the deployment with the new annotations
-        v1.patch_namespaced_deployment(name=dep.metadata.name, namespace=namespace_name, body=dep)
+        try:
+            # Update the deployment with the new annotations
+            v1.patch_namespaced_deployment(
+                name=dep.metadata.name, namespace=namespace_name, body=dep
+            )
 
-        print(f"Added keel annotation to deployment '{dep.metadata.name}' in the '{namespace_name}' namespace.")
+            print(
+                f"Added keel annotation to deployment '{dep.metadata.name}'"
+                + f"in the '{namespace_name}' namespace."
+            )
+        except ApiException:
+            print(
+                f"Annotation for '{dep.metadata.name}'"
+                + f"in the '{namespace_name}' namespace already added."
+            )

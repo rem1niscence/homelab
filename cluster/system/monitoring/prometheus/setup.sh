a61e7051cbb81109c9d06b1ebb62a9a3ba383cdf
Roniel Valdez <roniel_valdez@outlook.com>
2025-12-17 01:29:30 +0100

    feat: simplify prometheus values

--- cluster/system/monitoring/prometheus/setup.sh
+++ cluster/system/monitoring/prometheus/setup.sh
@@ -1,3 +1,5 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
-helm upgrade --install prometheus prometheus-community/kube-prometheus-stack  --namespace monitoring -f values.yml
+helm repo update
+helm upgrade --install prometheus prometheus-community/kube-prometheus-stack  -n monitoring \
+  -f values.yml --create-namespace --version 80.4.1

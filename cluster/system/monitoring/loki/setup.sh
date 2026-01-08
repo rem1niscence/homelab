e7dc7901575e3d1602e8d88d11a2122b6a09bf75
Roniel Valdez <roniel_valdez@outlook.com>
2026-01-08 02:41:52 +0100

    feat: monitoring ansible playbook

--- cluster/system/monitoring/loki/setup.sh
+++
@@ -1,4 +0,0 @@
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
-
-helm upgrade --install loki grafana/loki --namespace monitoring -f values.yml
--- cluster/system/monitoring/prometheus/setup.sh
+++
@@ -1,5 +0,0 @@
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-
-helm repo update
-helm upgrade --install prometheus prometheus-community/kube-prometheus-stack  -n monitoring \
-  -f values.yml --create-namespace --version 80.4.1
